e49aebadd62ec047edf16cfa36df736c124c9385
mihai_coanda <mihai_coanda@9fd5a63c-2584-0410-8418-171d5a423b52>
2015-09-08 10:38:26 +0000

    EXM-34369 made the github url connection throw a USER_ACTION_REQUIRED exception if the operation fails with a 404 response (github signals 404 is unauthorized for security reasons).
    git-svn-id: http://devel-new.sync.ro/svn/svnrepos/trunk/oxygen-sdk-samples/oxygen-sample-plugins/oxygen-sample-plugin-github@131986 9fd5a63c-2584-0410-8418-171d5a423b52

--- src/main/java/com/oxygenxml/sdksamples/github/GithubUrlConnection.java
+++ src/main/java/com/oxygenxml/sdksamples/github/GithubUrlConnection.java
@@ -10,7 +10,9 @@ import java.util.List;
 
 import org.jboss.resteasy.util.Base64;
 
+import ro.sync.ecss.extensions.api.webapp.WebappMessage;
 import ro.sync.ecss.extensions.api.webapp.plugin.FilterURLConnection;
+import ro.sync.ecss.extensions.api.webapp.plugin.UserActionRequiredException;
 import ro.sync.net.protocol.FileBrowsingConnection;
 import ro.sync.net.protocol.FolderEntryDescriptor;
 import ro.sync.util.URLUtil;
@@ -62,30 +64,46 @@ public class GithubUrlConnection extends FilterURLConnection implements FileBrow
   @Override
   public List<FolderEntryDescriptor> listFolder() throws IOException {
     List<FolderEntryDescriptor> filesList = new ArrayList<FolderEntryDescriptor>();
-    
-    String githubJsonResult = GithubUtil.inputStreamToString(delegateConnection.getInputStream());
-    
-    // If this is a Json array: [{content:'content'},{content:'content'},{content:'content'}]
-    if (githubJsonResult.charAt(0) == '[') {
-      List<GithubApiResult> githubResults = GithubUtil.parseGithubListResult(githubJsonResult);
-    
-      for (GithubApiResult result: githubResults) {
-        // Add a '/' when the file is a directory because this is how upstream identifies directories
-        String dirChar = "";
-        if (result.type.equals("dir")) {
-          dirChar = "/";
+    try {
+      String githubJsonResult = GithubUtil
+          .inputStreamToString(delegateConnection.getInputStream());
+
+      // If this is a Json array:
+      // [{content:'content'},{content:'content'},{content:'content'}]
+      if (githubJsonResult.charAt(0) == '[') {
+        List<GithubApiResult> githubResults = GithubUtil
+            .parseGithubListResult(githubJsonResult);
+
+        for (GithubApiResult result : githubResults) {
+          // Add a '/' when the file is a directory because this is how upstream
+          // identifies directories
+          String dirChar = "";
+          if (result.type.equals("dir")) {
+            dirChar = "/";
+          }
+
+          filesList.add(new FolderEntryDescriptor(URLUtil
+              .encodeURIComponent(result.name) + dirChar));
         }
-        
-        filesList.add(new FolderEntryDescriptor(URLUtil.encodeURIComponent(result.name) + dirChar));
+      } else {
+        // The result is a file and its content is Base64 encoded in the content
+        // property
+        GithubApiResult githubResult = GithubUtil
+            .parseGithubResult(githubJsonResult);
+
+        byte[] decodedContent = Base64.decode(githubResult.content);
+        filesList.add(new FolderEntryDescriptor(new String(decodedContent)));
       }
-    } else {
-      // The result is a file and its content is Base64 encoded in the content property
-      GithubApiResult githubResult = GithubUtil.parseGithubResult(githubJsonResult);
-      
-      byte[] decodedContent = Base64.decode(githubResult.content);
-      filesList.add(new FolderEntryDescriptor(new String(decodedContent)));
+    } catch (IOException e) {
+      if (e.getMessage().startsWith("404 Not Found")) {
+        // if the user is not authorized
+        throw new UserActionRequiredException(new WebappMessage(
+            WebappMessage.MESSAGE_TYPE_CUSTOM, "Authentication required",
+            "Authentication required", true));
+      } else {
+        throw e;
+      }
     }
-    
     return filesList;
   }
   
