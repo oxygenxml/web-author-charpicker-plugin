5a7ee6b0ec8a16f6c4819e0801bb601cc3fe9e7a
ggbt <gabriel_titerlea@sync.ro>
2015-11-11 06:15:00 +0000

    EXM-35008: Fixed a typo
    git-svn-id: http://devel-new.sync.ro/svn/svnrepos/trunk/oxygen-sdk-samples/oxygen-sample-plugins/oxygen-sample-plugin-github@134851 9fd5a63c-2584-0410-8418-171d5a423b52

--- src/main/java/com/oxygenxml/sdksamples/github/GithubPluginConfigExtension.java
+++ src/main/java/com/oxygenxml/sdksamples/github/GithubPluginConfigExtension.java
@@ -114,7 +114,7 @@ public class GithubPluginConfigExtension extends PluginConfigExtension {
     
     return "<div style='font-family: robotolight, Arial, Helvetica, sans-serif;font-size:0.9em;font-weight: lighter;'>"
             + "<div style='color:#333;background-color: lightyellow;border: 1px solid #dadab4;padding:0 5px 0 5px'>"
-              + "<p style='margin:8px 0'>The github plugin requires a Client ID and Client Secret to use OAuth authentication.</p>"
+              + "<p style='margin:8px 0'>The GitHub plugin requires a Client ID and Client Secret to use OAuth authentication.</p>"
               + "<p style='margin:8px 0'>To obtain them go to your github <a target='_blank' href='https://github.com/settings/applications/new'>developer applications page</a> and register a new application.</p>"
               + "<p style='margin:8px 0'>The Authorization callback URL must be set to: ${webapp-context}/plugins-dispatcher/github-oauth/callback"
               + "<p style='margin:8px 0'>Example:<br/><span style='text-decoration:underline'>http://${domain}:8080/oxygen-webapp/plugins-dispatcher/github-oauth/callback</p>"
