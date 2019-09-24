7776c985f3735aa85f5b386638d4255455ee1495
andreip1123 <andrei.popa1123@gmail.com>
2019-09-24 11:39:07 +0300

    WA-3306: Add char picker action to author type editors only.

--- web/plugin.js
+++ web/plugin.js
@@ -10,27 +10,29 @@
 
     function addToFrameworkToolbar(editor) {
       goog.events.listen(editor, sync.api.Editor.EventTypes.ACTIONS_LOADED, function (e) {
-        var actionsConfigToolbars = e.actionsConfiguration.toolbars;
+        if (editor.getEditorType() === sync.api.Editor.EditorTypes.AUTHOR) {
+          var actionsConfigToolbars = e.actionsConfiguration.toolbars;
 
-        var frameworkToolbar = null;
-        if (actionsConfigToolbars) {
-          for (var i = 0; i < actionsConfigToolbars.length; i++) {
-            var toolbar = actionsConfigToolbars[i];
-            if (toolbar.name !== "Review" && toolbar.name !== "Builtin") {
-              frameworkToolbar = toolbar;
+          var frameworkToolbar = null;
+          if (actionsConfigToolbars) {
+            for (var i = 0; i < actionsConfigToolbars.length; i++) {
+              var toolbar = actionsConfigToolbars[i];
+              if (toolbar.name !== "Review" && toolbar.name !== "Builtin") {
+                frameworkToolbar = toolbar;
+              }
             }
           }
-        }
-        // adds the action only once, on the first toolbar that is not Review or Builtin
-        if (frameworkToolbar && addActionOnce === 0) {
-          addActionOnce++;
-          frameworkToolbar.children.push({
-            id: insertSpecialCharActionId,
-            type: "action"
-          });
-          setTimeout(function () {
-            insertFromMenu.init();
-          }, 0);
+          // adds the action only once, on the first toolbar that is not Review or Builtin
+          if (frameworkToolbar && addActionOnce === 0) {
+            addActionOnce++;
+            frameworkToolbar.children.push({
+              id: insertSpecialCharActionId,
+              type: "action"
+            });
+            setTimeout(function () {
+              insertFromMenu.init();
+            }, 0);
+          }
         }
       });
     }
