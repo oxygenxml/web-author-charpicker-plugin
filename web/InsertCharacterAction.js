ae95d8130d6dee71cb50ecde2401f9014b8ba685
bogdan_paulon <bogdan_paulon@sync.ro>
2021-06-23 15:55:07 +0300

    WA-4940 schedule insert character action
    Execute the action with the scheduler and wait until
    the local documents gets updated

--- web/InsertCharacterAction.js
+++ web/InsertCharacterAction.js
@@ -79,6 +79,8 @@ InsertFromMenuAction.prototype.init = function () {
   // QUICK INSERT GRID
   goog.events.listen(document.querySelector('.goog-char-picker-grid'), goog.events.EventType.CLICK,
     goog.bind(this.quickInsertCharFromGrid_, this));
+  
+  this.actionsExecutor_ = this.editor_.getEditingSupport().actionsExecutor;
 };
 
 /**
@@ -389,13 +391,22 @@ InsertFromMenuAction.prototype.getLargeIcon = function () {
  * @private
  */
 InsertFromMenuAction.prototype.insertSpecialCharacterText_ = function (quickInsertChar) {
-  this.editor_.getActionsManager().invokeOperation(
-    'ro.sync.ecss.extensions.commons.operations.InsertOrReplaceTextOperation',
-    { text: quickInsertChar },
-    function () {
-      addNewRecentCharacters([quickInsertChar]);
-    }
-  );
+  InsertFromMenuAction.prototype.quickInsertCharFromGrid_ = function (e) {
+    this.actionsExecutor_.executeAction(new sync.actions.Action({
+      execute: () => {
+        return new Promise((resolve) => {
+          this.editor_.getActionsManager().invokeOperation(
+            'ro.sync.ecss.extensions.commons.operations.InsertOrReplaceTextOperation',
+            {text: quickInsertChar},
+            function () {
+              addNewRecentCharacters([quickInsertChar]);
+              resolve(quickInsertChar);
+            }
+          );
+        })
+      }
+    }));
+  };
 };
 
 /**
