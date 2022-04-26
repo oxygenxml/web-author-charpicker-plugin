c4aefeac96d1afa4de2efee6471375bc910e1448
andreip1123 <andrei.popa1123@gmail.com>
2022-04-26 19:06:58 +0300

    WA-5543: Fix for action getting added multiple times, better check for the framework actions loaded event.

--- web/plugin.js
+++ web/plugin.js
@@ -8,19 +8,22 @@
     var addActionOnce = 0;
     addToFrameworkToolbar(editor);
 
+    function toolbarContainsCharpicker (toolbar) {
+      return toolbar.children.find(element => element.id === insertSpecialCharActionId);
+    }
+
+    function isFrameworkActionsLoaded(e) {
+      var toolbars = e.actionsConfiguration.toolbars;
+      return toolbars && !toolbars.find(toolbar => toolbar.name === "Review" || toolbar.name === "Builtin");
+    }
+
     function addToFrameworkToolbar(editor) {
       goog.events.listen(editor, sync.api.Editor.EventTypes.ACTIONS_LOADED, function (e) {
-        if (editor.getEditorType() === sync.api.Editor.EditorTypes.AUTHOR) {
-          var actionsConfigToolbars = e.actionsConfiguration.toolbars;
-
-          var frameworkToolbar = null;
-          if (actionsConfigToolbars) {
-            for (var i = 0; i < actionsConfigToolbars.length; i++) {
-              var toolbar = actionsConfigToolbars[i];
-              if (toolbar.name !== "Review" && toolbar.name !== "Builtin") {
-                frameworkToolbar = toolbar;
-              }
-            }
+        if (editor.getEditorType() === sync.api.Editor.EditorTypes.AUTHOR && isFrameworkActionsLoaded(e)) {
+          var frameworkToolbar = e.actionsConfiguration.toolbars[0];
+          if(toolbarContainsCharpicker(frameworkToolbar)) {
+            // Action may already be added when changing editors without reload (if there's a custom toolbar)
+            addActionOnce++;
           }
           // adds the action only once, on the first toolbar that is not Review or Builtin
           if (frameworkToolbar && addActionOnce === 0) {
